84334f577bbebc2cd10d3afe5b804c57d6868849
wajidulkhan <wajidulkhan2001@gmail.com>
2025-08-15 19:12:52 +0600

    assainment 4 in javascript code problem solve

---
+++ solution.js
@@ -0,0 +1,28 @@
+// Problem-01 : Train TT's Fine Calculator
+
+// function totalFine(fare) {
+//   if (fare > 0 && typeof fare === "number") {
+//     const serviceCharge = 30;
+//     const fineCharge = (fare * 20) / 100;
+//     const fine = fare + serviceCharge + fineCharge;
+//     return fine;
+//   } else {
+//     return "invalid";
+//   }
+// }
+
+// Problem-02 : Clean & Capitalize Characters
+
+// function onlyCharacter(str) {
+//   if (typeof str !== "string") {
+//     return "invalid";
+//   }
+//   const removeSpace = str.replace(/\s/g, "");
+//   return removeSpace.toUpperCase();
+// }
+
+// 3Problem-03 : FIFA Best Team Award
+
+function bestTeam(player1, player2) {
+  // You have to write your code here
+}
